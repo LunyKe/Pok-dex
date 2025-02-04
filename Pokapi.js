01e862744cd2f9c52330afa107808f4b47c66dee
Jordan Campillo <jordancampillo@MacBook-Pro-de-Jordan.local>
2025-02-04 16:27:19 +0100

    new type

--- Pokapi.js
+++ Pokapi.js
@@ -25,6 +25,27 @@ const typeColors = {
     Fée: "linear-gradient(135deg, #C97BCE, #994E97)"
 };
 
+const multiColors = {
+    Normal: "#A8A878",
+    Feu: "#F08030",
+    Eau: "#6890F0",
+    Électrik: "#F8D030",
+    Plante: "#78C850",
+    Glace: "#98D8D8",
+    Combat: "#C03028",
+    Poison: "#A040A0",
+    Sol: "#E0C068",
+    Vol: "#A890F0",
+    Psy: "#F85888",
+    Insecte: "#A8B820",
+    Roche: "#B8A038",
+    Spectre: "#705898",
+    Dragon: "#7038F8",
+    Ténèbres: "#705848",
+    Acier: "#B8B8D0",
+    Fée: "#EE99AC"
+  };
+
 // On utilise fetch pour faire notre requete, par défaut fetch fait des req de type GET
 fetch(apiLink)
 .then(res => res.json())
@@ -53,10 +74,16 @@ fetch(apiLink)
         cardImage.src = imageURL
 
         // On vient chercher le premier type d'un pokémon pour lui attribuer sa couleur en background a partir de la table typeColors
-        if (types.length > 0) {
+        
+        if (types.length == 1) {
+            console.log(types.lenght)
             const firstType = types[0].name;
             card.style.background = typeColors[firstType] || "#ccc"; // "ccc" est au cas ou il y a un problème
-        };
+        } else {
+            const firstType = types[0].name;
+            const secType = types[1].name;
+            card.style.background = `linear-gradient(135deg, ${multiColors[firstType]}, ${multiColors[secType]})`
+        }
 
         types.forEach((type) => {
             cardTypes.textContent += `${type.name} `
